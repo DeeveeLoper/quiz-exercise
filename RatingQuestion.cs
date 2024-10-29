dc636d7b7c65d9135c50a2a2dc476456ee2a3e44
RobCam <roberto.camacho4@gmail.com>
2024-10-29 23:11:40 +0100

    Added new class RatingQuestion and method CheckAnswer

---
+++ RatingQuestion.cs
@@ -0,0 +1,24 @@
+// klassen ärver egenskaper och metoder från huvudklassen Question
+public class RatingQuestion : Question 
+{
+    private int correctRating { get; set; } // Rätt Svar
+
+    public RatingQuestion(string text, int points, int rating)
+        : base(text, points)
+    {
+        if (rating < 1 || rating > 10) 
+        {
+            throw new ArgumentException("Måste vara mellan 1-10!")
+        }
+        correctRating = rating;
+    }
+
+    public override bool CheckAnswer(string answer) 
+    {
+        if (int.TryParse(answer, out int givenRating)) 
+        {
+            return givenRating == correctRating; // kollar att användarens svar samma som rätta svaret 
+        }
+        return false;
+    }
+}
